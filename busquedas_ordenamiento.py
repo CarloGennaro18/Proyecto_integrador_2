978fcd99fd82e145466def7ea03197daf4d2ae7a
Carlo Barroso <carlobarroso06@gmail.com>
2025-10-18 16:28:46 -0400

    Algoritmos de ordenamiento tanto de burbuja como seleccion hechos

--- busquedas_ordenamiento.py
+++ busquedas_ordenamiento.py
@@ -27,3 +27,35 @@ def buscar_binaria_codigo(productos_ordenados, codigo_buscar):
         else:
             der = medio - 1
     return None
+def ordenar_burbuja(productos, clave, ascendente=True):
+    """Ordenamiento burbuja, para obtener el valor a comparar"""
+    n = len(productos)
+    
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if ascendente:
+                condicion = clave(productos[j]) > clave(productos[j + 1])
+            else:
+                condicion = clave(productos[j]) < clave(productos[j + 1])
+            
+            if condicion:
+                productos[j], productos[j + 1] = productos[j + 1], productos[j]
+
+
+def ordenar_seleccion(productos, clave, ascendente=True):
+    """Ordenamiento por seleccion para obtener el valor a comparar"""
+    n = len(productos)
+    
+    for i in range(n):
+        idx_seleccionado = i
+        
+        for j in range(i + 1, n):
+            if ascendente:
+                condicion = clave(productos[j]) < clave(productos[idx_seleccionado])
+            else:
+                condicion = clave(productos[j]) > clave(productos[idx_seleccionado])
+            
+            if condicion:
+                idx_seleccionado = j
+        
+        productos[i], productos[idx_seleccionado] = productos[idx_seleccionado], productos[i]
